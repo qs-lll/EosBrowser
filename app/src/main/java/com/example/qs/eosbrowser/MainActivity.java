2d081938f85eb5a0242289102d5420e3011fa4b0
qs-lll <l124578>
2018-11-09 21:25:40 +0800

    更改汇率

--- app/src/main/java/com/example/qs/eosbrowser/MainActivity.java
+++ app/src/main/java/com/example/qs/eosbrowser/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.qs.eosbrowser;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.FrameLayout;
 import android.widget.Toast;
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-        loadData();
+        loadData("game");
     }
 
     private void initView() {
@@ -35,15 +37,16 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().add(R.id.listfragment, listFragment).commit();
     }
 
-    private void loadData() {
+    private void loadData(final String account) {
         handler.postDelayed(new Runnable() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
             @Override
             public void run() {
 //                Exe.getAction("https://proxy.eosnode.tools", "gameinvestor", new Callback<ResAction>() {
-                Exe.getAction("http://192.168.0.107:8888", "q", new Callback<ResAction>() {
+                Exe.getAction("http://192.168.0.117:8888", account, new Callback<ResAction>() {
                     @Override
                     public void onResponse(Call<ResAction> call, Response<ResAction> response) {
-                        listFragment.setData(response.body(),"q");
+                        listFragment.setData(response.body(), account);
 
                     }
 
@@ -52,7 +55,9 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this, t.getMessage(), 0).show();
                     }
                 });
-                loadData();
+                if (!isDestroyed()) {
+                    loadData(account);
+                }
             }
         }, 3000);
 
